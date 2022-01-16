c5ea5545ce7cda2697f16359240f2d87da86890e
yamada-tak07 <faljojohsuke@gmail.com>
2022-01-17 01:23:42 +0900

    add prefix dictionary used in MVP(Minimal Viable Product).

---
+++ typeng/src/dictionary.ts
@@ -0,0 +1,22 @@
+interface dictionaryInterface {
+    bi: String[]
+    pedal: String[]
+    omni: String[]
+    ling: String[]
+    tox: String[]
+    terr: String[]
+    chron: String[]
+}
+
+
+const dictionary:dictionaryInterface = {
+    bi: ["bivalve", "bimonthly", "bicycle", "bifocal", "bilingual", "billion", "biannual", "bicentennial", "biped", "bisexual"],
+    pedal: ["pedestrian", "pedal", "pedestal", "pedicure", "biped"],
+    omni: ["omnivore", "omnisyncratic", "omnibus", "omnidirectional", "omnipotent", "omnipresent", "omniscient"],
+    ling: ["linguist", "monolingal", "bilingual", "trilingal", "linguistic"],
+    tox: ["detoxification", "toxic", "toxify", "toxicology", "intoxicated", "detox"],
+    terr: ["terrestrial", "extraterrestrial", "terra", "terraform", "terrarium", "territory", "terrain"],
+    chron: ["chronic", "chronicle", "chronograph", "chronological", "chronicler"],
+};
+
+export default dictionary;
