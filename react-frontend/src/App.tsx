9a4bf76f6d27d43bd7730a8cff03579b386f771c
yamada-tak07 <faljojohsuke@gmail.com>
2022-02-12 00:16:28 +0900

    Add HTTP Request method to activate lambda function.

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 
-import Sidebar from './components/side_bar';
+import Sidebar from './components/sideBar';
 import PrefixList from './components/prefixList';
 
 const gridStyle = {
